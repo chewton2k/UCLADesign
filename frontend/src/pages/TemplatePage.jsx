335e2a92acfa52372f4d879394e8a4cc094a5ea2
chewton2k <168577923+chewton2k@users.noreply.github.com>
2025-03-06 14:22:32 -0800

    fixed footer for template page

--- frontend/src/pages/TemplatePage.jsx
+++ frontend/src/pages/TemplatePage.jsx
@@ -4,13 +4,15 @@ import HomePageBottom from "../components/HomePageBottom";
 
 const TemplatePage = () => {
     return (
-        <>
+        <div>
             <HomePageTop/> 
             <div className="relative inset-0 flex items-center justify-center z-20" > <p className="font-bold text-s">
                 Templates will go here. 
                 </p></div>
+            {/* Separator for the last section and footer*/}
+            <div className="py-40"></div>
             <HomePageBottom/> 
-        </>
+        </div>
     )
 }
 
