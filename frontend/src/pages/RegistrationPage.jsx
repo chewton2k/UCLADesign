32dbfe1b084747fe5a288201020b1ef3e361dfd2
chewton2k <168577923+chewton2k@users.noreply.github.com>
2025-03-06 14:25:26 -0800

    added to the signup page

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -8,7 +8,7 @@ const RegistrationPage = () => {
         <div>
             <HomePageTop/> 
             <RegistrationForm/>
-            <div className="relative inset-0 flex items-center justify-center z-20" > <p className="font-bold text-s">
+            <div className="relative inset-0 flex items-center justify-center z-20" > <p className="font-light text-s">
                 By signing up you agree with our Terms of Service and Privacy Policy.
                 </p></div>
             {/* Separator for the last section and footer*/}
