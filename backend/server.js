cea529c70b4987382f9f396e3583357ec7add172
alexdlct <112514808+alexdlct@users.noreply.github.com>
2025-03-08 15:16:00 -0800

    typo in server.js fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/furnture", require("./routes/furntireRoutes"));
+app.use("/api/furnture", require("./routes/furnitureRoutes"));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
