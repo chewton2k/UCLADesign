30e9055e42f4703fa360654605a7231b9e032ab1
chewton2k <168577923+chewton2k@users.noreply.github.com>
2025-03-11 13:29:21 -0700

    moved todo list to s separate page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import TemplatePage from './pages/TemplatePage.jsx'
 //import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ProtectedRoutes from './utils/ProtectedRoutes.jsx'
 import CreateDesignPage from "./pages/CreateDesignPage.jsx";
+import TodoListPage from "./pages/TodoListPage.jsx";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                 <Route path='/login' element={<LogInPage/>}> </Route>
                 <Route path='/signup' element={<RegistrationPage/>}> </Route>
                 <Route path='/create-design' element={<CreateDesignPage/>}> </Route>
+                <Route path='/todo-list' element={<TodoListPage/>}> </Route>
 
                 {/* <Route element={<ProtectedRoutes/>}> 
                 <Route path='/create-design' element={<CreateDesignPage/>}> </Route>
--- frontend/src/pages/TodoListPage.jsx
+++ frontend/src/pages/TodoListPage.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import HomePageTop from '../components/HomePageTop';
 import HomePageBottom from "../components/HomePageBottom";
 import TodoListForm from '../components/TodoListForm';
-import CheckList from '../components/Checklist';
 
-const CreateDesignPage = () => {
+const TodoListPage = () => {
     return (
         <div>
             <HomePageTop /> 
             <main className="flex-grow"> 
             <div className="min-h-screen bg-gray-100 p-8">
-                 <CheckList/> 
+                 <h1 className="text-3xl font-bold text-center mb-6">My To-Do List</h1>
+                 <TodoListForm/> 
             </div>
                 {/* Separator for the last section and footer */}
                 <div className="py-50"></div>
@@ -20,4 +20,4 @@ const CreateDesignPage = () => {
     );
 }
 
-export default CreateDesignPage;
+export default TodoListPage;
