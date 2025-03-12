ac3e86a56a2b3271afeaa7ff9fda6900c65f47e2
alexdlct <112514808+alexdlct@users.noreply.github.com>
2025-03-11 20:31:19 -0700

    Created Room Designer grid component -- added to CreateDesignPage

--- frontend/src/pages/CreateDesignPage.jsx
+++ frontend/src/pages/CreateDesignPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePageTop from '../components/HomePageTop';
 import HomePageBottom from "../components/HomePageBottom";
 import Sidebar from '../components/Sidebar';
+import RoomDesigner from '../components/RoomDesigner';
 
 const CreateDesignPage = () => {
     return (
@@ -10,6 +11,9 @@ const CreateDesignPage = () => {
                 <main className="flex-grow">
                     <div className="min-h-screen bg-gray-100 flex">
                         <Sidebar />
+                        <div className="flex-grow">
+                            <RoomDesigner />
+                        </div>
                     </div>
                     <div className="py-50"></div>
                 </main>
