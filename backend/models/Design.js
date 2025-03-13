15f1b88bfe25a7cea34575fa55e2a5cb48f13313
owenrfn <owenrfan@gmail.com>
2025-03-12 18:15:10 -0700

    design schema change

--- backend/models/Design.js
+++ backend/models/Design.js
@@ -3,19 +3,10 @@ const mongoose = require("mongoose");
 const designSchema = new mongoose.Schema(
     {
         userName:{type: String, required: true},
-        userId:{type: Number, required: true},
         layout: [{
-            room: { type: String, required: true },
-            objects: [{
-                obj_id: {type: String, required: true},
-                name: {type: String, required: true},
-                position: {
-                    x: {type: Number, required: true},
-                    y: {type: Number, required: true},
-                }
-            }],
-            image: {type: String},
-        }],
+            id: Number, src: String, x: Number, y: Number,
+        },
+    ],
     }, {timestamps: true}
 );
 
