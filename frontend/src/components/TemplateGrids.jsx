18318d173d20ee9266010dad43845fe09b46e2d9
chewton2k <168577923+chewton2k@users.noreply.github.com>
2025-03-12 22:16:43 -0700

    fixed merge conflict with review button

--- frontend/src/components/TemplateGrids.jsx
+++ frontend/src/components/TemplateGrids.jsx
@@ -69,19 +69,10 @@ const TemplateGrid = () => {
                             </div>
 
                             <h3 className="text-lg font-semibold mb-2">{template.userName}'s Design</h3>
-                            <ReviewSys />
                             <HeartButton />
                         </div>
-<<<<<<< Updated upstream
-                        
-                        <h3 className="text-lg font-semibold mb-2">{template.userName}'s Design</h3>
-                        <HeartButton />
-                    </div>
-                ))
-=======
                     );
                 })
->>>>>>> Stashed changes
             )}
         </div>
     );
